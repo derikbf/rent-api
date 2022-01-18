b3e8716f14144a3772ece263df4d7db37b199f8d
derikbf <derikb@gmail.com>
2022-01-18 13:06:11 -0300

    Created Migration User

---
+++ db/migrate/20220118154922_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :avatar
+      t.string :name
+      t.string :username
+      t.string :email
+      t.string :password_digest
+      t.string :cpf
+      t.integer :phone
+
+      t.timestamps
+    end
+  end
+end
