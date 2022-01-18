7dbe2543dab65051854575d63a7d33183c8a7c82
derikbf <derikb@gmail.com>
2022-01-18 16:13:52 -0300

    Created migration Scaffold Category

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_18_184552) do
+ActiveRecord::Schema.define(version: 2022_01_18_190436) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.bigint "rental_company_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["rental_company_id"], name: "index_categories_on_rental_company_id"
+  end
 
   create_table "customer_addresses", force: :cascade do |t|
     t.string "country"
@@ -78,5 +87,6 @@ ActiveRecord::Schema.define(version: 2022_01_18_184552) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "categories", "rental_companies"
   add_foreign_key "customer_addresses", "customers"
 end
