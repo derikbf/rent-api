e0fe46e972a67178f3a89498c209f135b785cca4
derikbf <derikb@gmail.com>
2022-01-18 15:39:23 -0300

    Created User Consumer CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     namespace 'v1' do 
       
       resources :user_operators
+      resources :user_consumers
 
       resources :customers do
         resources :customer_address
---
+++ test/controllers/user_consumers_controller_test.rb
@@ -0,0 +1,38 @@
+require "test_helper"
+
+class UserConsumersControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user_consumer = user_consumers(:one)
+  end
+
+  test "should get index" do
+    get user_consumers_url, as: :json
+    assert_response :success
+  end
+
+  test "should create user_consumer" do
+    assert_difference('UserConsumer.count') do
+      post user_consumers_url, params: { user_consumer: {  } }, as: :json
+    end
+
+    assert_response 201
+  end
+
+  test "should show user_consumer" do
+    get user_consumer_url(@user_consumer), as: :json
+    assert_response :success
+  end
+
+  test "should update user_consumer" do
+    patch user_consumer_url(@user_consumer), params: { user_consumer: {  } }, as: :json
+    assert_response 200
+  end
+
+  test "should destroy user_consumer" do
+    assert_difference('UserConsumer.count', -1) do
+      delete user_consumer_url(@user_consumer), as: :json
+    end
+
+    assert_response 204
+  end
+end
