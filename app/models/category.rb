1548c3b8d852ed759e961f22f6b46e157c8db772
derikbf <derikb@gmail.com>
2022-01-18 16:13:35 -0300

    Created Model Category

---
+++ app/models/category.rb
@@ -0,0 +1,7 @@
+class Category < ApplicationRecord
+  belongs_to :rental_company
+
+  validates :name, presence: true, 
+                   :uniqueness => {:case_sensitive => false},
+                   length: { minimum: 3, maximum: 40 }
+end
