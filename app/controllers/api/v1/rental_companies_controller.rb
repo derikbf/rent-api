6042383f277b4a6cbc20a6a5e68f8a2e1eb23330
derikbf <derikb@gmail.com>
2022-01-18 15:51:52 -0300

    Created Company Rental Controller

---
+++ app/controllers/api/v1/rental_companies_controller.rb
@@ -0,0 +1,55 @@
+module Api
+	module V1
+    class RentalCompaniesController < ApplicationController
+      before_action :set_rental_company, only: [:show, :update, :destroy]
+
+      # GET /rental_companies
+      def index
+        @rental_companies = RentalCompany.all
+
+        render json: @rental_companies
+      end
+
+      # GET /rental_companies/1
+      def show
+        render json: @rental_company
+      end
+
+      # POST /rental_companies
+      def create
+        @rental_company = RentalCompany.new(rental_company_params)
+
+        if @rental_company.save
+          render json: @rental_company, status: :created
+        else
+          render json: @rental_company.errors, status: :unprocessable_entity
+        end
+      end
+
+      # PATCH/PUT /rental_companies/1
+      def update
+        if @rental_company.update(rental_company_params)
+          render json: @rental_company
+        else
+          render json: @rental_company.errors, status: :unprocessable_entity
+        end
+      end
+
+      # DELETE /rental_companies/1
+      def destroy
+        @rental_company.destroy
+      end
+
+      private
+        # Use callbacks to share common setup or constraints between actions.
+        def set_rental_company
+          @rental_company = RentalCompany.find(params[:id])
+        end
+
+        # Only allow a list of trusted parameters through.
+        def rental_company_params
+          params.require(:rental_company).permit(:id, :avatar, :corporate_name, :fantasy_name, :email, :cnpj, :phone, :cel_phone)
+        end
+    end
+  end
+end
