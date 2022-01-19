6c471210fa1cd7ce5c536ece688f911a50998f5b
derikbf <derikb@gmail.com>
2022-01-18 22:02:57 -0300

    Created Migration Auto Directions

---
+++ db/migrate/20220118220943_create_auto_directions.rb
@@ -0,0 +1,10 @@
+class CreateAutoDirections < ActiveRecord::Migration[6.1]
+  def change
+    create_table :auto_directions do |t|
+      t.integer :direction
+      t.references :rental_company, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
