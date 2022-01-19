7d69560a16f8127f422f2331ecb40b872375ad28
derikbf <derikb@gmail.com>
2022-01-19 19:36:19 -0300

    Updated Model Auto

--- app/models/auto.rb
+++ app/models/auto.rb
@@ -1,7 +1,6 @@
 class Auto < ApplicationRecord
   belongs_to :rental_company
   belongs_to :category
-  has_many :unavailable_periods
 
   validates :name, presence: true, 
                    :uniqueness => {:case_sensitive => false},
