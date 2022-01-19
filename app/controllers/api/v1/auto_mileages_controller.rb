c25b8714e75b441781fcfebbe6347fc2ab4b0c3a
derikbf <derikb@gmail.com>
2022-01-18 21:53:34 -0300

    Created Controller Auto Mileages

---
+++ app/controllers/api/v1/auto_mileages_controller.rb
@@ -0,0 +1,56 @@
+module Api
+	module V1
+    class AutoMileagesController < ApplicationController
+      before_action :set_auto_mileage, only: [:show, :update, :destroy]
+
+      # GET /auto_mileages
+      def index
+        @auto_mileages = AutoMileage.all
+
+        render json: @auto_mileages
+      end
+
+      # GET /auto_mileages/1
+      def show
+        render json: @auto_mileage
+      end
+
+      # POST /auto_mileages
+      def create
+        @auto_mileage = AutoMileage.new(auto_mileage_params)
+        @auto_mileage.rental_company = RentalCompany.find(params[:rental_company_id])
+
+        if @auto_mileage.save
+          render json: @auto_mileage, status: :created
+        else
+          render json: @auto_mileage.errors, status: :unprocessable_entity
+        end
+      end
+
+      # PATCH/PUT /auto_mileages/1
+      def update
+        if @auto_mileage.update(auto_mileage_params)
+          render json: @auto_mileage
+        else
+          render json: @auto_mileage.errors, status: :unprocessable_entity
+        end
+      end
+
+      # DELETE /auto_mileages/1
+      def destroy
+        @auto_mileage.destroy
+      end
+
+      private
+        # Use callbacks to share common setup or constraints between actions.
+        def set_auto_mileage
+          @auto_mileage = AutoMileage.find(params[:id])
+        end
+
+        # Only allow a list of trusted parameters through.
+        def auto_mileage_params
+          params.require(:auto_mileage).permit(:id, :mileage, :rental_company_id)
+        end
+    end
+  end
+end
