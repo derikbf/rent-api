2a4b8dca0a7c926f24db22f06bccb353bb80f241
derikbf <derikb@gmail.com>
2022-01-18 21:53:47 -0300

    Created Controller Auto Plates

---
+++ app/controllers/api/v1/auto_plates_controller.rb
@@ -0,0 +1,56 @@
+module Api
+	module V1
+    class AutoPlatesController < ApplicationController
+      before_action :set_auto_plate, only: [:show, :update, :destroy]
+
+      # GET /auto_plates
+      def index
+        @auto_plates = AutoPlate.all
+
+        render json: @auto_plates
+      end
+
+      # GET /auto_plates/1
+      def show
+        render json: @auto_plate
+      end
+
+      # POST /auto_plates
+      def create
+        @auto_plate = AutoPlate.new(auto_plate_params)
+        @auto_plate.rental_company = RentalCompany.find(params[:rental_company_id])
+
+        if @auto_plate.save
+          render json: @auto_plate, status: :created
+        else
+          render json: @auto_plate.errors, status: :unprocessable_entity
+        end
+      end
+
+      # PATCH/PUT /auto_plates/1
+      def update
+        if @auto_plate.update(auto_plate_params)
+          render json: @auto_plate
+        else
+          render json: @auto_plate.errors, status: :unprocessable_entity
+        end
+      end
+
+      # DELETE /auto_plates/1
+      def destroy
+        @auto_plate.destroy
+      end
+
+      private
+        # Use callbacks to share common setup or constraints between actions.
+        def set_auto_plate
+          @auto_plate = AutoPlate.find(params[:id])
+        end
+
+        # Only allow a list of trusted parameters through.
+        def auto_plate_params
+          params.require(:auto_plate).permit(:id, :name, :rental_company_id)
+        end
+    end
+  end
+end
