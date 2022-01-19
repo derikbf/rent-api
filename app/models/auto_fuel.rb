a4239cd1120fea98437ea4611ac3db49ffada36b
derikbf <derikb@gmail.com>
2022-01-18 23:17:28 -0300

    Updated Enum Auto Fuel

--- app/models/auto_fuel.rb
+++ app/models/auto_fuel.rb
@@ -1,5 +1,5 @@
 class AutoFuel < ApplicationRecord
   belongs_to :rental_company
 
-  enum status: { gasolina: 0, etanol: 1, gas: 2, diesel: 3 }
+  enum fuel: { gasolina: 0, etanol: 1, gas: 2, diesel: 3 }
 end
