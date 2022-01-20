8411177320cae44242531c802487127c88168f1c
derikbf <derikb@gmail.com>
2022-01-19 21:29:56 -0300

    Update Model Userconsumer with relationships

--- app/models/user_consumer.rb
+++ app/models/user_consumer.rb
@@ -1,4 +1,6 @@
 class UserConsumer < ApplicationRecord
+  belongs_to :rental_company
+
   validates :email, presence: true, uniqueness: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
