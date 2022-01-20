52ef56d68d14a0d40e8ad5a199c08cc110b48934
derikbf <derikb@gmail.com>
2022-01-19 21:31:00 -0300

    Update migration Create Booking with: news fields: status total subtotal perc discount discount

--- db/migrate/20220119222442_create_bookings.rb
+++ db/migrate/20220119222442_create_bookings.rb
@@ -3,8 +3,14 @@ class CreateBookings < ActiveRecord::Migration[6.1]
     create_table :bookings do |t|
       t.date :start_date
       t.date :end_date
+      t.integer :status
+      t.decimal :total, precision: 9, scale: 2
+      t.decimal :subtotal, precision: 9, scale: 2
+      t.decimal :perc_discount, precision: 9, scale: 2
+      t.decimal :discount, precision: 9, scale: 2
       t.references :auto, null: false, foreign_key: true
-      t.references :customer, null: false, foreign_key: true
+      t.references :customer, null: true, foreign_key: true
+      t.references :user_consumer, null: true, foreign_key: true
 
       t.timestamps
     end
