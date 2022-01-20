f3871079a17576751ba772900de17d0ea5a82893
derikbf <derikb@gmail.com>
2022-01-19 21:30:10 -0300

    Update Model User Operator with relationships

--- app/models/user_operator.rb
+++ app/models/user_operator.rb
@@ -1,4 +1,6 @@
 class UserOperator < ApplicationRecord
+  belongs_to :rental_company
+
   validates :email, presence: true, uniqueness: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
