c1bb42f444f9bf936ca4c8bbabc72a0c5054fcb3
derikbf <derikb@gmail.com>
2022-01-20 08:23:22 -0300

    Created Migration Rating

---
+++ db/migrate/20220120111005_create_ratings.rb
@@ -0,0 +1,11 @@
+class CreateRatings < ActiveRecord::Migration[6.1]
+  def change
+    create_table :ratings do |t|
+      t.integer :star
+      t.string :description
+      t.references :booking, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
