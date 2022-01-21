828281e2bfc4bf7ee7324e66ece3e03ff5868a50
derikbf <derikb@gmail.com>
2022-01-21 14:30:06 -0300

    Add Pagination in Auto Optionala

--- app/controllers/api/v1/auto_optionals_controller.rb
+++ app/controllers/api/v1/auto_optionals_controller.rb
@@ -6,7 +6,7 @@ module Api
 
       # GET /auto_optionals
       def index
-        @auto_optionals = AutoOptional.all
+        @auto_optionals = AutoOptional.order("created_at DESC").page(params[:page].try(:[], :number))
 
         render json: @auto_optionals
       end
