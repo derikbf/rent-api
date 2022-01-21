6f03211956a54bbcf7a1b96e50003ae3fd40e586
derikbf <derikb@gmail.com>
2022-01-20 22:13:53 -0300

    Updated Controller Auto Optional With Serializer

--- app/controllers/api/v1/auto_optionals_controller.rb
+++ app/controllers/api/v1/auto_optionals_controller.rb
@@ -1,6 +1,7 @@
 module Api
 	module V1
     class AutoOptionalsController < ApplicationController
+      include ErrorSerializer
       before_action :set_auto_optional, only: [:show, :update, :destroy]
 
       # GET /auto_optionals
@@ -23,7 +24,7 @@ module Api
         if @auto_optional.save
           render json: @auto_optional, status: :created
         else
-          render json: @auto_optional.errors, status: :unprocessable_entity
+          render json: ErrorSerializer.serialize(@auto_optional.errors), status: :unprocessable_entity 
         end
       end
 
@@ -32,7 +33,7 @@ module Api
         if @auto_optional.update(auto_optional_params)
           render json: @auto_optional
         else
-          render json: @auto_optional.errors, status: :unprocessable_entity
+          render json: ErrorSerializer.serialize(@auto_optional.errors), status: :unprocessable_entity 
         end
       end
 
