0de41ef61ffb768ab1ba0212d557a04d001740f2
derikbf <derikb@gmail.com>
2022-01-20 22:29:06 -0300

    Created Serializer Booking

---
+++ app/serializers/booking_serializer.rb
@@ -0,0 +1,5 @@
+class BookingSerializer < ActiveModel::Serializer
+  attributes :id, :user_consumer_id, :customer_id, :auto_id, :start_date, :end_date, :total
+
+  link(:self) {api_v1_booking_url(object.id)}
+end
