9d1125a88d120c994d42fc7b98e0be9cc8b3069b
derikbf <derikb@gmail.com>
2022-01-28 17:35:21 -0300

    Inserted Validate Autos - Database Level

--- db/migrate/20220118192739_create_autos.rb
+++ db/migrate/20220118192739_create_autos.rb
@@ -1,8 +1,8 @@
 class CreateAutos < ActiveRecord::Migration[6.1]
   def change
     create_table :autos do |t|
-      t.string :name
-      t.string :description
+      t.string :name, null: false, limit: 60
+      t.string :description, limit: 120
       t.string :image_1_path
       t.decimal :unit_price, precision: 9, scale: 2
       t.references :rental_company, null: false, foreign_key: true
