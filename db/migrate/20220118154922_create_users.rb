1c959beb083eb32bf16170b1ced75ee1998505ac
derikbf <derikb@gmail.com>
2022-01-28 17:31:09 -0300

    Inserted Validate - Database Level

--- db/migrate/20220118154922_create_users.rb
+++ db/migrate/20220118154922_create_users.rb
@@ -2,12 +2,12 @@ class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :user_operators do |t|
       t.string :avatar
-      t.string :name
-      t.string :username
-      t.string :email
+      t.string :name, null: false, limit: 60
+      t.string :username, null: false, limit: 60
+      t.string :email, null: false, limit: 50
       t.string :password_digest
-      t.string :cpf
-      t.integer :phone
+      t.string :cpf, null: false, lenght: { is: 11 }
+      t.string :phone, null: false, limit: 15
 
       t.timestamps
     end
