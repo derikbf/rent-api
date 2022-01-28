71c9013a703c9422e720d7cb00db7d62e6c2cea4
derikbf <derikb@gmail.com>
2022-01-28 17:28:15 -0300

    Updated Model - validates User Consumer

--- app/models/user_consumer.rb
+++ app/models/user_consumer.rb
@@ -3,6 +3,13 @@ class UserConsumer < ApplicationRecord
   
   belongs_to :rental_company
 
-  validates :email, presence: true, uniqueness: true
+  validates :email, :username, :cpf, :phone, presence: true, uniqueness: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  
+  validates :name, :username, length: { minimum: 3, maximum: 60 }
+  validates :email, length: { minimum: 3, maximum: 50 }
+  validates :phone, length: { minimum: 11, maximum: 15 }, numericality: { only_integer: true }
+  validates :cpf, length: { is: 11 }, numericality: { only_integer: true }
 end
+
+
