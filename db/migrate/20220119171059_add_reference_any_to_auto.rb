d257115956a981e3cc17ebc95d3d653e892a75fd
derikbf <derikb@gmail.com>
2022-01-28 17:40:09 -0300

    Inserted Validate Auto reference - Database Level

--- db/migrate/20220119171059_add_reference_any_to_auto.rb
+++ db/migrate/20220119171059_add_reference_any_to_auto.rb
@@ -2,18 +2,18 @@ class AddReferenceAnyToAuto < ActiveRecord::Migration[6.1]
   def change
     add_reference :autos, :category, null: true, foreign_key: true
     add_reference :autos, :auto_brand, null: true, foreign_key: true
-    add_reference :autos, :auto_types, null: true, foreign_key: true
-    add_reference :autos, :auto_years, null: true, foreign_key: true
-    add_reference :autos, :auto_mileages, null: true, foreign_key: true
-    add_reference :autos, :auto_powers, null: true, foreign_key: true
-    add_reference :autos, :auto_fuels, null: true, foreign_key: true
+    add_reference :autos, :auto_type, null: true, foreign_key: true
+    add_reference :autos, :auto_year, null: true, foreign_key: true
+    add_reference :autos, :auto_mileage, null: true, foreign_key: true
+    add_reference :autos, :auto_power, null: true, foreign_key: true
+    add_reference :autos, :auto_fuel, null: true, foreign_key: true
     add_reference :autos, :auto_exchange, null: true, foreign_key: true
-    add_reference :autos, :auto_directions, null: true, foreign_key: true
-    add_reference :autos, :auto_colors, null: true, foreign_key: true
-    add_reference :autos, :auto_doors, null: true, foreign_key: true
-    add_reference :autos, :auto_plates, null: true, foreign_key: true
-    add_reference :autos, :auto_models, null: true, foreign_key: true
-    add_reference :autos, :auto_optionals, null: true, foreign_key: true
+    add_reference :autos, :auto_direction, null: true, foreign_key: true
+    add_reference :autos, :auto_color, null: true, foreign_key: true
+    add_reference :autos, :auto_door, null: true, foreign_key: true
+    add_reference :autos, :auto_plate, null: true, foreign_key: true
+    add_reference :autos, :auto_model, null: true, foreign_key: true
+    add_reference :autos, :auto_optional, null: true, foreign_key: true
     
     add_reference :user_operators, :rental_company, null: false, foreign_key: true
     add_reference :user_consumers, :rental_company, null: false, foreign_key: true
