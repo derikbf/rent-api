eda8a36ab2156c05566686e7bef9bafe3f328cf3
derikbf <derikb@gmail.com>
2022-01-28 17:26:34 -0300

    Updated Model - validates Auto Model

--- app/models/auto_model.rb
+++ app/models/auto_model.rb
@@ -3,7 +3,7 @@ class AutoModel < ApplicationRecord
   
   belongs_to :rental_company
 
-  validates :name, presence: true, uniqueness: :true
-
-  validates :name, length: { minimum: 3, maximum: 70 }
+  validates :name, presence: true, 
+                   :uniqueness => {:case_sensitive => false},
+                   length: { minimum: 2, maximum: 60 }
 end
