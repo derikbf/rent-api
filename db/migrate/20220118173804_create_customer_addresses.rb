607a5f243c80b15255ba320175ff3e079ec4ae82
derikbf <derikb@gmail.com>
2022-01-28 17:34:11 -0300

    Inserted Validate Customer Address - Database Level

--- db/migrate/20220118173804_create_customer_addresses.rb
+++ db/migrate/20220118173804_create_customer_addresses.rb
@@ -1,14 +1,14 @@
 class CreateCustomerAddresses < ActiveRecord::Migration[6.1]
   def change
     create_table :customer_addresses do |t|
-      t.string :country
-      t.string :uf
-      t.string :city
-      t.string :district
-      t.string :complement
-      t.string :number
-      t.string :street
-      t.string :cep
+      t.string :country, null: false, limit: 50
+      t.string :uf, null: false, limit: 50
+      t.string :city, null: false, limit: 50
+      t.string :district, null: false, limit: 50
+      t.string :complement, null: true, limit: 50
+      t.integer :number, null: false, limit: 6
+      t.string :street, null: false, limit: 50
+      t.integer :cep, null: false, lenght: { is: 8 }
       t.references :customer, null: false, foreign_key: true
 
       t.timestamps
