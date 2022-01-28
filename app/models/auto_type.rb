4e43dc65d47aa9771e307a3387d7a5be21ffa418
derikbf <derikb@gmail.com>
2022-01-28 17:27:05 -0300

    Updated Model - validates Auto Type

--- app/models/auto_type.rb
+++ app/models/auto_type.rb
@@ -2,4 +2,8 @@ class AutoType < ApplicationRecord
   acts_as_paranoid
   
   belongs_to :rental_company
+
+  validates :name, presence: true, 
+                   :uniqueness => {:case_sensitive => false},
+                   length: { minimum: 2, maximum: 60 }
 end
