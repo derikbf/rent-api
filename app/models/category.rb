e92d2efdc278225ef50144bd53a7365fd6a1937e
derikbf <derikb@gmail.com>
2022-01-28 17:27:46 -0300

    Updated Model - validates Category

--- app/models/category.rb
+++ app/models/category.rb
@@ -6,4 +6,5 @@ class Category < ApplicationRecord
   validates :name, presence: true, 
                    :uniqueness => {:case_sensitive => false},
                    length: { minimum: 3, maximum: 40 }
+
 end
