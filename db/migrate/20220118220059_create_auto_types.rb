b17e5e9ee15a0a8e2409c52f2d9cc5076ceead58
derikbf <derikb@gmail.com>
2022-01-28 17:36:22 -0300

    Inserted Validate Auto Type - Database Level

--- db/migrate/20220118220059_create_auto_types.rb
+++ db/migrate/20220118220059_create_auto_types.rb
@@ -1,7 +1,7 @@
 class CreateAutoTypes < ActiveRecord::Migration[6.1]
   def change
     create_table :auto_types do |t|
-      t.string :name
+      t.string :name, null: false, limit: 60
       t.references :rental_company, null: false, foreign_key: true
 
       t.timestamps
