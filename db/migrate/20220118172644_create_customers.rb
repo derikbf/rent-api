f5b9d09e49e844a8e9f02fed2cddf831af03fb6a
derikbf <derikb@gmail.com>
2022-01-28 17:32:23 -0300

    Inserted Validate - Database Level

--- db/migrate/20220118172644_create_customers.rb
+++ db/migrate/20220118172644_create_customers.rb
@@ -2,12 +2,12 @@ class CreateCustomers < ActiveRecord::Migration[6.1]
   def change
     create_table :customers do |t|
       t.string :avatar
-      t.string :corporate_name
-      t.string :fantasy_name
-      t.string :email
-      t.string :cnpj
-      t.string :phone
-      t.string :cel_phone
+      t.string :corporate_name, null: false, limit: 60
+      t.string :fantasy_name, null: false, limit: 60
+      t.string :email, null: false, limit: 50
+      t.string :cnpj, null: false, lenght: { is: 14 }
+      t.string :phone, null: false, limit: 15
+      t.string :cel_phone, null: false, limit: 15
 
       t.timestamps
     end
