3665954b44854c8005fb2b6dd4845044aa1c3605
derikbf <derikb@gmail.com>
2022-01-30 17:51:14 -0300

    Created Spec Validade Model Auto Plate

---
+++ spec/models/auto_plate_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe AutoPlate, type: :model do
+  subject(:auto_plate) { build :auto_plate }
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    # it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
+    it { is_expected.to validate_length_of(:name).is_equal_to(7) }
+  end
+  
+  describe 'relationships' do
+    it { is_expected.to belong_to(:rental_company) } 
+  end
+end
