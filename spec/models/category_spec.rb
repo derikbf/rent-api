a308c944196ff74c0254fb919eb957bb8d9a6241
derikbf <derikb@gmail.com>
2022-01-30 14:10:33 -0300

    Updated Spec Model Category

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -5,5 +5,11 @@ RSpec.describe Category, type: :model do
 
   describe 'validations' do
     it { should validate_presence_of(:name) }
+    # it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
+    it { is_expected.to validate_length_of(:name).is_at_least(3).is_at_most(40) }
+  end
+
+  describe 'relationships' do
+    it { is_expected.to belong_to(:rental_company) } 
   end
 end
