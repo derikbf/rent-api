eb20ab6f1647c90dad3f2d39c664b68a741272ff
derikbf <derikb@gmail.com>
2022-01-30 17:43:46 -0300

    Updated User Operator Model

--- app/models/user_operator.rb
+++ app/models/user_operator.rb
@@ -3,6 +3,7 @@ class UserOperator < ApplicationRecord
   
   belongs_to :rental_company
 
+  validates :name, presence: true
   validates :email, :username, :cpf, :phone, presence: true, uniqueness: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
   
