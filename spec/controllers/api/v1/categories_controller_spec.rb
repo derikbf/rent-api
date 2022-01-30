990777f083098df9171fc4215c628e277ebab09d
derikbf <derikb@gmail.com>
2022-01-30 17:45:05 -0300

    Created Categories Controller SPEC

--- spec/controllers/api/v1/categories_controller_spec.rb
+++ spec/controllers/api/v1/categories_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::CategoriesController, type: :controller do
-  # xdescribe "GET index" do
+  # describe "GET index" do
   #   it 'has a 200 status code' do
   #     get :index
   #     expect(response.status).to eq(200)
